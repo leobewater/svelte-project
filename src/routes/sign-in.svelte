637370c2132e9ec350247625ec00a1f9dd0b1dd0
Leo <lcheung@swellbottle.com>
2022-02-28 10:54:58 -0500

    add sign in and sign up routes

---
+++ src/routes/sign-in.svelte
@@ -0,0 +1,28 @@
+<script>
+  import SignInForm from '$lib/components/SignInForm.svelte'
+  let error
+
+  async function handleSubmit({ detail: { email, password } }) {
+    const response = await fetch('/api/sign-in', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (!response.ok) {
+      error = (await response.json()).message
+      return
+    }
+
+    // @ts-ignore
+    window.location = '/protected'
+  }
+</script>
+
+<h1 class="text-2xl font-semibold text-center">Sign In</h1>
+{#if error}
+  <p class="mt-3 text-red-500 text-center font-semibold">{error}</p>
+{/if}
+<SignInForm class="max-w-xl mx-auto mt-8" on:submit={handleSubmit} />
